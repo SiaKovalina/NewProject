6d229455a6acbfca3217cb382703b3ee57a1b283
SiaKovalina <siakovalina@gmail.com>
2017-05-05 16:51:03 +0300

    cool iphone animation finished

---
+++ js/custom.js
@@ -0,0 +1,22 @@
+$(function(){
+    $('.js-wp-2').waypoint(function(direction) {
+        $('.js-wp-2').addClass('animated slideInUp');
+        
+    }, {
+        offset: '70%'
+    });
+    
+    $('.js-wp-3').waypoint(function(direction) {
+        $('.js-wp-3').addClass('animated fadeIn');
+        
+    }, {
+        offset: '70%'
+    });
+    
+    $('.iphone-btn').delay(2300).animate({bottom: '+=-3'}, 300);
+    /*There s 2.3s delay from the page refresh.
+    The selected div gets animated to the bottom by 3px after the 2.3s delay and animation duration is 300ms*/
+    
+    $('.iphone-btn').delay(300).animate({top: '+=-3'}, 300);
+    /*This delay takes place after the previous animation is finished*/
+});
